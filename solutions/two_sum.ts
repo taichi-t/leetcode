89d1503ccf36e02bb51b33ab232a96fd609024e1
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-11-06 20:19:00 -0800

    :fire: Remove comments.

--- solutions/two_sum.ts
+++ solutions/two_sum.ts
@@ -1,10 +1,8 @@
 export default function twoSum(nums: number[], target: number): number[] {
   const map = new Map<number, number>();
-  //look speed in map is 0(1)
   for (let i = 0; i < nums.length; i++) {
     let currVal: number = nums[i];
     if (map.has(currVal)) {
-      //we find the diff that adds up to target.
       console.log(map.get(currVal) as number, i);
       return [map.get(currVal), i];
     }
