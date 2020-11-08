6a97c92bfbab473225e9173905593427293b1114
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-11-08 00:19:08 -0800

    :white_check_mark: Add test case for two_sum

--- solutions/first_unique_char.test.ts
+++ solutions/first_unique_char.test.ts
@@ -1,16 +1,16 @@
 import firstUniqChar from './first_unique_char';
 
-// const str1 = 'leetcode';
-// const str2 = 'loveleetcode';
+const str1 = 'leetcode';
+const str2 = 'loveleetcode';
 const str3 = 'dddccdbba';
 
-// test('first unique char 1', () => {
-//   expect(firstUniqChar(str1)).toEqual(0);
-// });
+test('first unique char 1', () => {
+  expect(firstUniqChar(str1)).toEqual(0);
+});
 
-// test('first unique char 2', () => {
-//   expect(firstUniqChar(str2)).toEqual(2);
-// });
+test('first unique char 2', () => {
+  expect(firstUniqChar(str2)).toEqual(2);
+});
 
 test('first unique char 3', () => {
   expect(firstUniqChar(str3)).toEqual(8);
